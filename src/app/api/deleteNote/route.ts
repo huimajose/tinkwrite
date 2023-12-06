447abc42806aff8a0635304f429009ac04585b0e
Huima Jose <imoicalda@gmail.com>
2023-12-06 12:15:44 +0100

    Added delete button

--- src/app/api/deleteNote/route.ts
+++ src/app/api/deleteNote/route.ts
@@ -2,9 +2,16 @@ import { db } from "@/lib/db";
 import { $notes } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
 import { NextResponse } from "next/server";
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient('https://iehsmuxjlrzfwordijiy.supabase.co','eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllaHNtdXhqbHJ6ZndvcmRpaml5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEzMzkzNjcsImV4cCI6MjAxNjkxNTM2N30.8hmf2igDjxqcd6WH0LgxLhhzp1z5ll4TZ1hTEiKYRYM');
+  
 
 export async function POST(req: Request) {
   const { noteId } = await req.json();
-  await db.delete($notes).where(eq($notes.id, parseInt(noteId)));
+  const { error } = await supabase
+  .from('notes')
+  .delete()
+  .eq('id', noteId)
   return new NextResponse("ok", { status: 200 });
 }
