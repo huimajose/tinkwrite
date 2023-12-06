017050c4b3b0af71aeaf15bc9f020c9c683141f4
Huima Jose <imoicalda@gmail.com>
2023-12-06 17:02:06 +0100

    Added localizations portuguese default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import Provider from "@/components/Provider";
+import { ptBR } from "@clerk/localizations";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider localization={ptBR}>
       <html lang="en">
       <head>
         <link rel="manifest" href="/manifest.json" />
