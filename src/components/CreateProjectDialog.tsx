1cef6f79a87743d0c21540bf3a7f4f7dd6babba2
Huima Jose <imoicalda@gmail.com>
2023-12-11 10:33:25 +0100

    Added dialogAddProject

---
+++ src/components/CreateProjectDialog.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { } from "./ui/dialog"
+import { Loader2, Plus } from "lucide-react";
+import { Input } from "./ui/input";
+import axios from "axios";
+import { Button } from "./ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@clerk/nextjs";
+
+import React from 'react'
+
+interface Props {
+    
+}
+
+
+
+const CreateProjectDialog = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default CreateProjectDialog
