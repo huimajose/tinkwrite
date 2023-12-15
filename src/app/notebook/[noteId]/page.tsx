58cb46b05df850a537a523f24da6bbc5cd7d148b
Huima Jose <imoicalda@gmail.com>
2023-12-15 16:10:12 +0100

    Changed new tab in menu

--- src/app/notebook/[noteId]/page.tsx
+++ src/app/notebook/[noteId]/page.tsx
@@ -94,7 +94,7 @@ const NotebookPage = async ({ params: { noteId } }: Props) => {
         <MenubarTrigger>Ficheiro</MenubarTrigger>
         <MenubarContent>
           <MenubarItem>
-            New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+           Nova janela <MenubarShortcut>⌘T</MenubarShortcut>
           </MenubarItem>
           <MenubarItem>
             New Window <MenubarShortcut>⌘N</MenubarShortcut>
